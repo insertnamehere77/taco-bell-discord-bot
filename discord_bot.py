964272217cd997f218b89710d9dc1ffdf36a1d8c
insertnamehere77 <Kaleeb@kalebs-mbp.lan>
2019-08-11 01:17:02 -0500

    Added some comments

--- discord_bot.py
+++ discord_bot.py
@@ -7,12 +7,13 @@ class DiscordBot():
 	_URL = 'https://discordapp.com/api/'
 
 	"""docstring for DiscordBot"""
-	def __init__(self, user, token):
-		self.username = user
+	def __init__(self, id, token):
+		self.user_id = id
 		self.token = token
 
 		self.socket = None
 		self._heartbeat_task = None
+
 
 	def __del__(self):
 		if self.socket:
@@ -23,6 +24,53 @@ class DiscordBot():
 
 
 
+	# POST a message for the users to see
+	def send_msg(self, channel_id, msg, tts = False):
+		payload = {
+		  "content": msg,
+		  "tts" : tts
+		}
+
+		headers = self._headers()
+		msg_url = '{}channels/{}/messages'.format(DiscordBot._URL, channel_id)
+		response = requests.post(msg_url, data = payload, headers = headers)
+
+		return response.json()
+
+
+	# PUT a reaction on the specified message
+	def add_reaction(self, channel_id, msg_id, reaction):
+		headers = self._headers()
+		reaction_url = '{}channels/{}/messages/{}/reactions/{}/@me'.format(DiscordBot._URL, channel_id, msg_id, reaction)
+		response = requests.put(reaction_url, headers = headers)
+
+
+
+	# Connects to the socket and acts on events
+	async def listen(self):
+
+		await self._connect()
+		
+		self._heartbeat_task = asyncio.create_task(self._heartbeat())
+
+
+
+		while True:
+			event_str = await self.socket.recv()
+			event = json.loads(event_str)
+			print("NEW MSG", event)
+			print("\n")
+
+			if event['t'] == 'MESSAGE_CREATE':
+				self._msg_created(event['d'])
+
+
+			if event['op'] == 9:
+				await self._identify()
+
+
+
+	# Connects to the websocket and identifies itself
 	async def _connect(self):
 		gateway = self._get_gateway()
 		url = gateway['url']
@@ -36,6 +84,8 @@ class DiscordBot():
 		await self._identify()
 
 
+	# Function to send the websocket a heartbeat every 30 seconds
+	# Should be ran as an async task
 	async def _heartbeat(self):
 		
 		while True:
@@ -46,14 +96,15 @@ class DiscordBot():
 
 
 
-
-	def _create_headers(self):
+	# Creates the header for any HTTP calls
+	def _headers(self):
 		headers = {
 			"Authorization" : 'Bot ' + self.token
 		}
 		return headers
 
 
+	# Sends the socket our identity payload so it knows who TF we are
 	async def _identify(self):
 		payload = {
 			"token": self.token,
@@ -68,88 +119,58 @@ class DiscordBot():
 		await self.socket.send(msg)
 
 
+	# Wraps a payload to send to our websocket
 	def _wrap_payload(self, opcode, payload):
-			msg = {
-			  "op": opcode,
-			  "d": payload
-			}
-
-			return json.dumps(msg)
-
-
-	def send_msg(self, room_id, msg, tts = False):
-		payload = {
-		  "content": msg,
-		  "tts" : tts
+		msg = {
+		  "op": opcode,
+		  "d": payload
 		}
 
-		headers = self._create_headers()
-		msg_url = '{}channels/{}/messages'.format(DiscordBot._URL, room_id)
-		response = requests.post(msg_url, data = payload, headers = headers)
-
-		return response.json()
+		return json.dumps(msg)
 
 
-	def add_reaction(self, room_id, msg_id, reaction):
-		headers = self._create_headers()
-		reaction_url = '{}channels/{}/messages/{}/reactions/{}/@me'.format(DiscordBot._URL, room_id, msg_id, reaction)
-		response = requests.put(reaction_url, headers = headers)
 
 
+	# GET the gateway URL so we can connect
 	def _get_gateway(self):
-		headers = self._create_headers()
+		headers = self._headers()
 
 		response = requests.get('https://discordapp.com/api/gateway/bot', headers = headers)
 		return response.json()
 
 
+	# Scans the mentions to see if our bot got called
 	def _mentioned(self, mentions):
 		for user in mentions:
-			if user['username'] == self.username:
+			if user['id'] == self.user_id:
 				return True
 
 		return False
 
 
+	# The function that handles a MESSAGE_CREATED event
 	def _msg_created(self, payload):
 
 		if self._mentioned(payload['mentions']):
-			room_id = payload['channel_id']
-			self.send_msg(room_id, 'Think outside the bun')
+			channel_id = payload['channel_id']
+
+			author_id = payload['author']['id']
+
+			msg = 'Think outside the bun <@{}>'.format(author_id)
+
+			self.send_msg(channel_id, msg, True)
 
 			msg_id = payload['id']
 			reactions = ["🥙", "🌶️", "🇲🇽", "🌯", "🌮"]
 			reaction = random.choice(reactions)
 			print(reaction)
-			self.add_reaction(room_id, msg_id, reaction)
+			self.add_reaction(channel_id, msg_id, reaction)
 			return
 
 
 
 
 
-	async def listen(self):
-
-		await self._connect()
-		
-		self._heartbeat_task = asyncio.create_task(self._heartbeat())
-
-		while True:
-			msg_str = await self.socket.recv()
-			msg = json.loads(msg_str)
-			print("NEW MSG", msg)
-			print("\n")
-
-			if msg['t'] == 'MESSAGE_CREATE':
-				self._msg_created(msg['d'])
-				
-
-
-
-
-
-			if msg['op'] == 9:
-				await self._identify()
 
 
 
@@ -164,8 +185,8 @@ async def main():
 	config.read('bot_config.cfg')
 	
 	token = config['discord']['token']
-	username = config['discord']['username']
-	bot = DiscordBot(username, token)
+	user_id = config['discord']['user_id']
+	bot = DiscordBot(user_id, token)
 
 	await bot.listen()
 
